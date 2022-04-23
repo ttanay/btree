4f1ae3afb80222de5c8b7d4e8353bf8513f1dc43
Tanay Tummalpalli <ttanay100@gmail.com>
2022-04-23 07:49:42 +0530

    Insert working

--- btree.h
+++ btree.h
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <memory>
 
 struct BTreeNode {
   int n; // no of keys stored in the node
   std::vector<int> keys; // list of `n` keys
   bool isLeaf; // whether the node is a leaf
-  std::vector<BTreeNode*> children; // Array of the BTree Node's children
+  std::vector<std::shared_ptr<BTreeNode>> children; // Array of the BTree Node's children
 
-  public:
   //default constructor
   BTreeNode() {
     n = 0;
@@ -15,134 +15,46 @@ struct BTreeNode {
   }
 };
 
+using BTreeNodePtr = std::shared_ptr<BTreeNode>;
+
 struct BTreeSearchResult {
   BTreeNode node;
   int index;
 };
 
 class BTree {
-  int t; // The minimum_degree
-
-  BTreeSearchResult _search(BTreeNode*, int);
-  void split_child(BTreeNode*, int);
-  void _insert(BTreeNode*, int);
-
   public:
 
+  int t; // The minimum_degree
+
+  // Check whether the node is full(`n = 2*t - 1`)
+  bool is_full(BTreeNodePtr node);
+  BTreeSearchResult _search(BTreeNodePtr node, int element);
+  void split_child(BTreeNodePtr node, int index);
+  void split_root();
+  void _insert(BTreeNodePtr node, int element);
+  std::string _to_string(BTreeNodePtr node);
+
   // TODO: Make this private. This is public for test.
-  BTreeNode* root;
+  BTreeNodePtr root;
 
   BTree(int minimum_degree) {
     if(minimum_degree < 2)
       throw std::invalid_argument("minimum_degree should be >= 2");
     t = minimum_degree;
-    root = new BTreeNode();
+    root = std::make_unique<BTreeNode>();
   }
 
-  ~BTree() {
-    delete root;
-  }
+  BTreeSearchResult search(int element);
+  void insert(int element);
 
-  BTreeSearchResult search(int);
-  void insert(int);
+  /** String representation of a BTree is like the follows:
+   *   BTree([{3, 5, 8}, [[{1, 2}], [{4}], [{6, 7}], [{10}, [{9}], [{12, 14}]]]])
+   *
+   *   Here, each array is a node.
+   *   The first element enclosed in `{}` in the array are the keys.
+   *   The second element enclosed in `[]` are the child nodes in order.
+   *   The child nodes are in the same format.
+   */
+  std::string to_string();
 };
-
-BTreeSearchResult BTree::search(int element) {
-  return _search(root, element);
-}
-
-BTreeSearchResult BTree::_search(BTreeNode* node, int element) {
-  int i = 0;
-  // Find appropriate index where the key is likely to be found
-  while(i < node->n && node->keys[i] < element)
-    i++;
-
-  if(node->keys[i] == element)
-    return BTreeSearchResult {*node, i};
-  else if(node->isLeaf)
-    return BTreeSearchResult {};
-  else
-    return _search(node->children[i], element);
-}
-
-void BTree::insert(int element) {
-  if(root->n = (2 * t - 1)) {
-    BTreeNode* new_root = new BTreeNode();
-    new_root->isLeaf = false;
-    new_root->children[0] = root;
-    root = new_root;
-    split_child(root, 0);
-  }
-
-  //insert non-root
-  _insert(root, element);
-}
-
-void BTree::_insert(BTreeNode* node, int element) {
-  if(node->isLeaf) {
-    // The assumption here is that the node will always be non-full
-    //  => n < 2t - 1
-    int i = node->n - 1;
-
-    // Move elements greater than element one space ahead
-    for(; i > 0 && node->keys[i] > element; i--)
-      node->keys[i+1] = node->keys[i];
-
-    // Assign value
-    node->keys[i] = element;
-  } else {
-    // Find the appropriate location
-    int i = 0;
-    while(i < node->n && node->keys[i] <= element)
-      i++;
-
-    // Split child if node is full
-    if(node->children[i]->n == ((2 * t) - 1)) {
-      split_child(node, i);
-      // Increment index in case the key propogated up from the split
-      // is larger than the previous index
-      if (node->keys[i] > element)
-        i += 1;
-    }
-
-    // Insert in child node
-    _insert(node->children[i], element);
-  }
-}
-
-void BTree::split_child(BTreeNode* node, int index) {
-  BTreeNode* new_child = new BTreeNode();
-  BTreeNode* child = node->children[index];
-
-  new_child->isLeaf = child->isLeaf;
-  new_child->n = t - 1;
-
-  // Copy over keys at indices [t-1, 2t-1) to new node
-  for(int i = -1; i < t - 1; i++)
-    new_child->keys[i] = child->keys[t + i];
-
-  if(!child->isLeaf) {
-    // Copy over child nodes from indices [t, 2t)
-    for(int i = 0; i < t; i++)
-      new_child->children.push_back(
-        child->children[i+t]
-      );
-  }
-
-  // Set size of child to t-1
-  child->n = t - 1;
-
-
-  // Move node's children pointers right one index
-  // The last index for children is n+1
-  // We want to move elements in (index, n+1] to the right
-  for(int i = node->n+1; i > index; i--) {
-    node->children[i+1] = node->children[i];
-  }
-  // Move node's keys to the right by one index
-  for(int i = node->n; i >= index; i--) {
-    node->keys[i+1] = node->keys[i];
-  }
-  // move median key from child up into parent
-  node->keys[index] = child->keys[t-1];
-}
