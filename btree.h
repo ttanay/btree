c5c35203692fef97c5062a2a8705e9c8e45a30d7
Tanay Tummalpalli <ttanay100@gmail.com>
2022-05-03 05:59:20 +0530

    Search works

--- btree.h
+++ btree.h
@@ -2,14 +2,19 @@
 #include <vector>
 #include <memory>
 
-struct BTreeNode {
-  int n; // no of keys stored in the node
-  std::vector<int> keys; // list of `n` keys
-  bool isLeaf; // whether the node is a leaf
-  std::vector<std::shared_ptr<BTreeNode>> children; // Array of the BTree Node's children
+struct BTreeNode
+{
+  // no of keys stored in the node
+  int n;
+  // list of `n` keys
+  std::vector<int> keys;
+  // whether the node is a leaf
+  bool isLeaf;
+  // Array of the BTree Node's children
+  std::vector<std::shared_ptr<BTreeNode>> children;
 
-  //default constructor
-  BTreeNode() {
+  BTreeNode()
+  {
     n = 0;
     isLeaf = true;
   }
@@ -17,29 +22,37 @@ struct BTreeNode {
 
 using BTreeNodePtr = std::shared_ptr<BTreeNode>;
 
-struct BTreeSearchResult {
-  BTreeNode node;
+struct BTreeSearchResult
+{
+  BTreeNodePtr node;
+  int height;
   int index;
+
+  bool empty()
+  {
+    return (!height && !index);
+  }
 };
 
-class BTree {
-  public:
-
-  int t; // The minimum_degree
+class BTree
+{
+  // The minimum_degree
+  int t;
+  // The root of the tree
+  BTreeNodePtr root;
 
   // Check whether the node is full(`n = 2*t - 1`)
   bool is_full(BTreeNodePtr node);
-  BTreeSearchResult _search(BTreeNodePtr node, int element);
+  BTreeSearchResult _search(BTreeNodePtr node, int element, int height);
   void split_child(BTreeNodePtr node, int index);
   void split_root();
   void _insert(BTreeNodePtr node, int element);
   std::string _to_string(BTreeNodePtr node);
 
-  // TODO: Make this private. This is public for test.
-  BTreeNodePtr root;
-
-  BTree(int minimum_degree) {
-    if(minimum_degree < 2)
+public:
+  BTree(int minimum_degree)
+  {
+    if (minimum_degree < 2)
       throw std::invalid_argument("minimum_degree should be >= 2");
     t = minimum_degree;
     root = std::make_unique<BTreeNode>();
@@ -49,11 +62,8 @@ class BTree {
   void insert(int element);
 
   /** String representation of a BTree is like the follows:
-   *   BTree([{3, 5, 8}, [[{1, 2}], [{4}], [{6, 7}], [{10}, [{9}], [{12, 14}]]]])
-   *
+   *   BTree(keys={5},children={[keys={3},children={}],[keys={8},children={}]})
    *   Here, each array is a node.
-   *   The first element enclosed in `{}` in the array are the keys.
-   *   The second element enclosed in `[]` are the child nodes in order.
    *   The child nodes are in the same format.
    */
   std::string to_string();
