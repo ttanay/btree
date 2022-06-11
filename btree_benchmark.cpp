1a12b4f76354d6a39336caf3068b5c373b7e23b9
Tanay Tummalpalli <ttanay100@gmail.com>
2022-06-12 01:19:34 +0530

    Add simple benchmark for the BTree
    TODO: Add comprehensive benchmarks
      Performance changes can be observed in an automated way
      with new optimizations, changes, etc.

---
+++ btree_benchmark.cpp
@@ -0,0 +1,19 @@
+// BTree benchmarks
+// 1. Insert
+// 2. Delete
+// 3. Search
+
+#include <vector>
+#include <benchmark/benchmark.h>
+
+#include "btree.h"
+
+static void BM_BtreeCreation(benchmark::State & state)
+{
+  for (auto _ : state)
+    BTree{2, std::vector<int>{1, 3, 5, 4, 7, 8, 9, 2, 6, 10, 12}};
+}
+
+BENCHMARK(BM_BtreeCreation);
+
+BENCHMARK_MAIN();
